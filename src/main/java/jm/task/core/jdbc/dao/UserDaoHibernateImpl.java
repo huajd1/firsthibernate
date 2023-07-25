e486c5381bd10551f05ae708f7a27ae5fd130c1f
hrrer <130406103+huajd1@users.noreply.github.com>
2023-07-25 15:31:18 +0300

    Add files via upload

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,118 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    private Session session;
+
+
+    public UserDaoHibernateImpl() {
+        session = Util.getSessionFactory().openSession();
+    }
+
+    @Override
+    public void createUsersTable() {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT NOT NULL AUTO_INCREMENT," +
+                    "name VARCHAR(45) NOT NULL," +
+                    "lastName VARCHAR(45) NOT NULL," +
+                    "age TINYINT NOT NULL," +
+                    "PRIMARY KEY (id)" +
+                    ")").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE FROM User WHERE id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList = null;
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("FROM User").list();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return userList;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+}
